9d508deaaf992a98240ee2bc93f2edd679b8a99e
Hugo <hugoguzman@gmail.com>
2021-01-06 09:55:03 -0500

    attempted fix to dependency error v1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import { useQuery } from 'react-query'
-import axios from 'axios'
-import Header from './Header'
-import ZipField from './ZipField'
-import Map from './Map'
-import List from './List'
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import Header from './Header';
+import ZipField from './ZipField';
+import Map from './Map';
+import List from './List';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,26 +16,40 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
-  const [zip, setZip] = useState('')
-  const [showList, setShowList] = useState(false)
-  const fetchFemaClaims = () => axios.get(process.env.REACT_APP_AWS_URL, { params: { zip } })
-  const rcResp = useQuery('fetchFemaClaims', fetchFemaClaims, { enabled: false })
-
-  console.log('rcResp', rcResp)
-  useEffect(() => {
-    if(zip) {
-      rcResp.refetch();
-      setShowList(false)
+  const [zip, setZip] = useState('');
+  const [showList, setShowList] = useState(false);
+  const fetchFemaClaims = () =>
+    axios.get(process.env.REACT_APP_AWS_URL, { params: { zip } });
+  const { status, data, error, isFetching, refetch } = useQuery(
+    'fetchFemaClaims',
+    fetchFemaClaims,
+    {
+      enabled: false,
     }
-  }, [zip])
+  );
+
+  console.log('data', data);
+  useEffect(() => {
+    if (zip) {
+      refetch();
+      setShowList(false);
+    }
+  }, [zip, refetch]);
 
   return (
     <div className={classes.root}>
       <Header />
       <ZipField setZip={setZip} />
       <Grid container spacing={3} className={classes.gridContainer}>
-        <Map {...rcResp} setShowList={setShowList} />
-        <List {...rcResp} showList={showList} />
+        <Map
+          status={status}
+          data={data}
+          error={error}
+          isFetching={isFetching}
+          refetch={refetch}
+          setShowList={setShowList}
+        />
+        <List data={data} showList={showList} />
       </Grid>
     </div>
   );
